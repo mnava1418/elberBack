91cee717630e7cbd283762369290fc8bca410be7
mnava1418 <mnavapena@gmail.com>
2019-12-09 21:01:29 -0600

    create register service

--- server/models/mongoose/User.js
+++ server/models/mongoose/User.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const SALT_WORK_FACTOR = 10;
-
 const UserSchema = mongoose.Schema({
     email: { type: String, required: true, index: { unique: true }, lowercase: true, trim: true },
     name: { type: String, required: true, trim: true },
@@ -11,18 +9,6 @@ const UserSchema = mongoose.Schema({
     phone: { type: String, required: true, trim: true },
     img: { type: String, required: false, trim: true },
 }, { timestamps: true });
-
-UserSchema.pre('save', (next) => {
-    const user = this;
-
-    if(!user.isModified('password'))
-        return next();
-
-    const hashPassword = bcrypt.hashSync(user.password, SALT_WORK_FACTOR);
-    user.password = hashPassword;
-    
-    return next();
-})
 
 UserSchema.methods.comparePassword = (password, hashPassword) => {
   return bcrypt.compareSync(password, hashPassword);
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+const UserModel = require('../models/mongoose/User')
+
 const login = (req, res) => {
     res.send('Login Service');
 }
@@ -6,8 +11,43 @@ const logout = (req, res) => {
     res.send('Logout Service');
 }
 
-const register = (req, res) => {
-    res.send('Register Service')
+const _getUser = async (email) => {
+    const user = await UserModel.findOne({ email: email});
+    return user
+}
+
+const register = async(req, res) => {
+    const existingUser = await _getUser(req.body.email);
+
+    if(existingUser) {
+        res.status(500);
+        return res.json({errMessage: `El email ${req.body.email} ya está registrado`});
+    }
+
+    const newUser = new UserModel(req.body)
+
+    if(!validator.isEmail(newUser.email)) {
+        res.status(500);
+        return res.json({errMessage: 'Email inválido!'});
+    }
+
+    if(!validator.equals(newUser.gender, 'Male') && !validator.equals(newUser.gender, 'Female')) {
+        res.status(500);
+        return res.json({errMessage: 'Genero inválido!'});
+    }
+
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+    newUser.password = hashPassword;
+    
+    newUser.save((err, user) => {
+        if(err){
+            res.status(500);
+            return res.json({errMessage: err.message});
+        } else {
+            user.password = undefined;
+            return res.json(user);
+        }
+    })
 }
 
 module.exports = () => {
