18005694dc687484b0819dafb322d6b169b6c2cd
mnava1418 <mnavapena@gmail.com>
2020-01-27 22:35:36 -0600

    changes to socketIO service

--- server/services/socketIO.js
+++ server/services/socketIO.js
@@ -3,17 +3,15 @@ var io = require('socket.io')
 module.exports = (server) => {
     io = io(server)
     
-    //Init  socket.io
     io.on('connection', (socket) => {
-    console.log(`a user connected to socket: ${socket.id}`);
-
-    socket.on('disconnect', () => {
-        console.log(`a user disconnected from: ${socket.id}`)
-    })
-
-    socket.on('elber request', (message) =>{
-        console.log(message)
-        io.to(`${socket.id}`).emit('elber response', 'Que pedo cabron!');
-    } )
+        console.log(`a user connected to socket: ${socket.id}`);
+      
+        socket.on('disconnect', () => {
+            console.log(`a user disconnected from socket: ${socket.id}`)
+        })
+      
+        socket.on('elber request', (message) =>{
+            io.to(`${socket.id}`).emit('elber response', message);
+        } )
     });
 }
