fee1ef466bc0de9b7b901564af5e4ec8e0cb61f1
mnava1418 <mnavapena@gmail.com>
2020-02-13 19:08:45 -0600

    changing confing

---
+++ server/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mail: {
+        messages: {
+            welcome:{
+                saludo: '<h3>Hola ',
+                mensaje: '!</h3<br><h5>Gracias por registrarte, seremos grandes amigos :) <br><br> Tu código de activación es: ',
+                footer: '<br><br>Atte. Elber Gun</h5>'
+            }
+        }
+    }
+}
