b731a780b3c38d5955088875bb8f2eb233d51757
mnava1418 <mnavapena@gmail.com>
2020-02-20 20:45:29 -0600

    remove resSendError function

--- server/controllers/utilityController.js
+++ server/controllers/utilityController.js
@@ -1,20 +1,15 @@
 const utilityService = require('../services/utilityService');
 const config = require('../config/index')
 
-const resErrorMessage = (res, errCode, errMessage ) => {
-    res.status(errCode).json({errMessage: errMessage})
-}
-
 const validateSourceApp = (req,res,next) => {
     if(utilityService.validateSourceApp(req.body.source)) {
         next();
     } else {
         const accessDeniedError = config.errorMessages.userService.accessDenied;
-        resErrorMessage(res, accessDeniedError.code, accessDeniedError.errMessage )
+        res.status(accessDeniedError.code).json({errMessage: accessDeniedError.errMessage})
     }
 }
 
 module.exports = {
-    validateSourceApp,
-    resErrorMessage
+    validateSourceApp
 }
