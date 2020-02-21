fcbd84ef47f81684e18abd5ff05eab1c1088b8a8
mnava1418 <mnavapena@gmail.com>
2020-02-20 20:46:27 -0600

    add invalidEmail entry

--- server/config/index.js
+++ server/config/index.js
@@ -14,6 +14,10 @@ module.exports = {
                 code: 401,
                 errMessage: 'Acceso denegado'
             },
+            invalidEmail: {
+                code: 500,
+                errMessage: 'El email no es válido'
+            }
         }
     },
     userStatus: {
