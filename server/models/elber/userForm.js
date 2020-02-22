580b7e86d34d9f25030c2b765c164394ddc5cb89
mnava1418 <mnavapena@gmail.com>
2020-02-21 19:57:18 -0600

    create userForm

---
+++ server/models/elber/userForm.js
@@ -0,0 +1,55 @@
+const validator = require('validator');
+
+let _email = undefined
+let _name = undefined
+let _password = undefined
+let _img = undefined
+let _actCode = undefined
+let _isActive = undefined
+
+class UserForm {
+    constructor(reqUserBody) {
+        _email = reqUserBody.email,
+        _name = reqUserBody.name,
+        _password = reqUserBody.password,
+        _img = reqUserBody.img,
+        _actCode = reqUserBody.actCode,
+        _isActive = reqUserBody.isActive
+    }
+
+    getUserAsObject() {
+        const userAsObject = {
+            email: _email,
+            name: _name,
+            password: _password,
+            img: _img,
+            actCode: _actCode,
+            isActive: _isActive
+        }
+
+        return userAsObject
+    }
+
+    getEmail() {
+        return _email
+    }
+
+    getPassword() {
+        return _password
+    }
+
+    setActCode(actCode) {
+        _actCode = actCode
+    }
+
+    setPassword(password) {
+        _password = password
+    }
+
+    isEmailValid()
+    {
+        return(validator.isEmail(_email))
+    } 
+}
+
+module.exports = UserForm
