e541b376df6af90789f18c4e913b1601b919f326
mnava1418 <mnavapena@gmail.com>
2020-02-21 19:56:39 -0600

    generate activation code

--- server/services/utilityService.js
+++ server/services/utilityService.js
@@ -1,4 +1,5 @@
 const appAuth = require("../config/appAuth")
+const crypto = require('crypto');
 
 const validateSourceApp = (source) => {
     if( source === appAuth.app.iosName) {
@@ -8,6 +9,17 @@ const validateSourceApp = (source) => {
     return false
 }
 
+const generateActivationCode = () => {
+    let actCode = crypto.randomBytes(6).toString('hex')
+
+    if(actCode.length > 8) {
+        actCode = actCode.substr(0,8)
+    }
+
+    return actCode
+}
+
 module.exports = {
-    validateSourceApp
+    validateSourceApp,
+    generateActivationCode
 }
