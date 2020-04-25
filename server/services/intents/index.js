26481ee4934ea61f7748695fda5b5d1e7874075b
mnava1418 <mnavapena@gmail.com>
2020-04-24 20:19:50 -0500

    process elber responses

---
+++ server/services/intents/index.js
@@ -0,0 +1,21 @@
+const intents = require('../../config/intents')
+const processParameters = require('./processParameters')
+const dateServices = require('../dateService')
+
+const getResponse = async(intent, parameters, fulfillmentText) => {
+    let response = "No se que quieres"
+    switch (intent) {
+      case intents.hora :
+          const location = processParameters.getLocation(parameters)
+          response = await dateServices.getTime(location)
+          break;
+      default:
+        response = fulfillmentText
+        break;
+    }
+    return response
+}
+
+module.exports = {
+    getResponse
+}
