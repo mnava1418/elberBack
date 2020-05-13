f6ac72f69158f8773862964307048bb87410dc5f
mnava1418 <mnavapena@gmail.com>
2020-05-13 18:32:40 -0500

    add logs

--- server/services/elberService.js
+++ server/services/elberService.js
@@ -20,13 +20,15 @@ const callIntent = async (email, message) => {
   const responses = await sessionClient.detectIntent(request)
   console.log('Respuesta recibida de elber. Procesando...')
   const result = responses[0].queryResult
-  const intent = result.intent.displayName.toUpperCase()
+  const intent = result.intent.displayName.toLowerCase()
   const parameters = result.parameters.fields
   const fulfillmentText = result.fulfillmentText
   const response = await intents.getResponse(email, intent, parameters, fulfillmentText)
 
   console.log(` Query: ${result.queryText}`);
-  console.log(` Response: ${response}`);
+  console.log(` Response: ${response.elberResponse}`);
+  console.log(` Next Action: ${response.nextAction}`);
+  console.log(` Local Function: ${response.localFunction}`);
 
   return response;
 }
