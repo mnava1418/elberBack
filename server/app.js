09996e125f28c90c182337932534d18e19837e9f
mnava1418 <mnavapena@gmail.com>
2020-08-17 12:31:09 -0500

    delete file for certificate

--- server/app.js
+++ server/app.js
@@ -26,8 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static('public'));
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+app.use(express.static('public'));
 app.use(limiter);
 
 //app routes
