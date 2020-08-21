c31848da1587bddd1f3a1480c5250cc30b941370
mnava1418 <mnavapena@gmail.com>
2020-08-21 18:42:27 -0500

    refactor app.js

--- server/app.js
+++ server/app.js
@@ -6,54 +6,61 @@ const logger = require('morgan');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const indexRouter = require('./routes');
-
-//DOS Attacks
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-
 const app = express();
+
+const setMiddlewares = () => {
+  //DOS Attacks parameters
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  });
+
+  //app middlewares
+  app.use(limiter);
+  app.use(helmet());
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static('public'));
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next()
+  })
+}
+
+const setViews = () => {
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
+}
+
+const setRoutes = () => {
+  //app routes
+  app.use('/', indexRouter());
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    if(err.status == 404 )
+      res.locals.message = 'Oops! Esta página no existe'
+    else
+      res.locals.message = 'Mi papá ya la cagó pero está trabajando en ello';
+    
+    res.locals.error = err;
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
+
 app.locals.title = 'Elber';
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-//app middlewares
-app.use(helmet());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static('public'));
-app.use(limiter);
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next()
-})
-
-//app routes
-app.use('/', indexRouter());
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  if(err.status == 404 )
-    res.locals.message = 'Oops! Esta página no existe'
-  else
-    res.locals.message = 'Mi papá ya la cagó pero está trabajando en ello';
-  
-  res.locals.error = err;
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+setViews();
+setMiddlewares();
+setRoutes();
 module.exports = app;
