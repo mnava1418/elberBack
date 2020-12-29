e8a428f37bdc1c11026e7984e4118d2208605640
mnava1418 <mnavapena@gmail.com>
2020-12-28 19:32:56 -0600

    delete unused files

--- server/models/mongoose/faceBookUserModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FBUserSchema = mongoose.Schema({
-    email: { type: String, required: true, index: { unique: true }, lowercase: true, trim: true },
-    name: { type: String, required: true, trim: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('FBUser', FBUserSchema);
