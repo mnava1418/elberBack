8d5686c242269da4017ebee3fb5652a9da48b503
mnava1418 <mnavapena@gmail.com>
2020-12-30 17:00:39 -0600

    delete unused scripts

--- server/models/mongoose/skillModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SkillSchema = mongoose.Schema({
-    name: { type: String, required: true, trim: true },
-    icon: { type: String, required: true, trim: true },
-    demo: { type: Array, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Skill', SkillSchema);
