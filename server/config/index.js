bda8f16eda17f9eccac56fec023ab2104c8337a2
mnava1418 <mnavapena@gmail.com>
2020-12-30 17:03:48 -0600

    remove unused logic

--- server/config/index.js
+++ server/config/index.js
@@ -4,14 +4,6 @@ module.exports = {
             accessDenied: {
                 code: 401,
                 errMessage: 'Acceso denegado'
-            },
-            invalidEmail: {
-                code: 500,
-                errMessage: 'El email no es válido'
-            },
-            invalidUser: {
-                code: 401,
-                errMessage: 'Email/Password incorrecto'
             }
         }
     }
--- server/config/intents.js
+++ server/config/intents.js
@@ -4,16 +4,6 @@ module.exports = {
         hora: 'date.currenttime',
         fecha: 'date.currentdate'
     },
-    lists: {
-        id: 'lists',
-        getLists: 'lists.getall',
-        createList: 'lists.create',
-        deleteList: 'lists.delete',
-        addItems: 'lists.additems',
-        listContent: 'lists.getlistcontent',
-        removeItem: 'lists.removeitem',
-        clearList: 'lists.clearlist'
-    },
     spotify: {
         id: 'spotify',
         play: 'spotify.play',
