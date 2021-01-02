99701ace8dda274b624d12e9e7bfe3ed4f872797
mnava1418 <mnavapena@gmail.com>
2021-01-02 11:48:39 -0600

    get historic data

--- server/services/cryptoService.js
+++ server/services/cryptoService.js
@@ -6,27 +6,56 @@ const getPrice = async (from, to, type) => {
     const result = await networkService.restCall(HOST,path, 'GET', {}, undefined)
     .then(response => {
         if(response.data) {
-            return response.data.amount
+            let data = {}
+            if(type.includes('date')) {
+                const date = type.split('date=')[1]
+                data[date] = response.data.amount
+            } else {
+                data[type] = response.data.amount
+            }
+
+            return data
         }
-        
-        return 'N/A'
+        return {}
     })
     .catch(err => {
         console.log(err)
-        return 'N/A'
+        {}
     })
 
     return result
 }
 
 const getCurrencyInfo = async (from, to) => {
-    const spotPrice = await getPrice(from, to, 'spot')
-    const buyPrice = await getPrice(from, to, 'buy')
-    const sellPrice = await getPrice(from, to, 'sell')
+    let calls = [getPrice(from, to, 'spot'), getPrice(from, to, 'buy'), getPrice(from, to, 'sell')]
+    const priceTypes = ['spot', 'buy', 'sell']
+    let date = new Date()
 
-    console.log(`Spot Price: ${spotPrice}`)
-    console.log(`Buy Price: ${buyPrice}`)
-    console.log(`Sell Price: ${sellPrice}`)
+    for(let i = 0; i < 30; i++) {
+        if(i > 0) {
+            date.setDate(date.getDate() - 1);
+        }
+
+        let finalDate = date.toISOString().split('T')[0]
+        calls.push(getPrice(from, to, `spot?date=${finalDate}`))
+    }
+
+    const result = await Promise.all(calls)
+    let info = {}
+    let history = {}
+    
+    for(let entry of result) {
+        for(let priceType in entry) {
+            if(priceTypes.indexOf(priceType) >= 0) {
+                info[priceType] = entry[priceType]
+            } else {
+                history[priceType] = entry[priceType]
+            }
+        }
+    }
+
+    info.history = history
+    return info
 }
 
 module.exports = {
