f500d892e2825df9e90b86d2601f128ff24e51af
mnava1418 <martin@mnavapena.com>
2024-03-29 12:48:43 -0400

    UPDATE: send new user message to kafka

--- user-services/src/config/auth.js
+++ user-services/src/config/auth.js
@@ -5,5 +5,13 @@ module.exports = {
     fireBase: {
         credential: process.env.FIREBASE_CERTIFICATE,
         dbURL: process.env.FIREBASE_DB_URL
+    },
+
+    kafka: {
+        clientId: 'user-services',
+        brokers: [process.env.KAFKA_HOST],
+        topics: {
+            email: 'email-events'
+        }
     }
 }
