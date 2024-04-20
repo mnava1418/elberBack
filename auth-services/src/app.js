4455e4e79295e11d941229b7e415a7f00f91d098
mnava1418 <martin@mnavapena.com>
2024-04-20 17:57:20 -0400

    ADD: DOS attack protection

--- auth-services/src/app.js
+++ auth-services/src/app.js
@@ -2,18 +2,25 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const rateLimit = require('express-rate-limit')
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-const setMIddlewares = () => {
+const setMiddlewares = () => {
   // view engine setup
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
+  
+  //DOS Attacks parameters
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000 // limit each IP to 1000 requests per windowMs
+  });
 
-  app.use(logger('dev'));
+  //app middlewares
+  app.use(limiter);
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
@@ -40,7 +47,7 @@ const setRoutes = () => {
   });
 }
 
-setMIddlewares()
+setMiddlewares()
 setRoutes()
 
 module.exports = app;
