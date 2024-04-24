2336e9ef40f45c5c730b62ce9cfb195f2b1baefb
mnava1418 <martin@mnavapena.com>
2024-04-24 19:44:05 -0400

    UPDATE: generate to tokens . One to  accept and to for reject

--- auth-services/src/services/userService.js
+++ auth-services/src/services/userService.js
@@ -1,12 +1,17 @@
 const tokenService = require('./tokenService')
 const {sendMessage, topics} = require('kafka-services')
 
+const REQUEST_ACCEPT_ACTION = 'accept'
+const REQUEST_REJECT_ACTION = 'reject'
+
 const requestRegistrationCode = async(email) => {
     const payload = {sender: email}
-    const token = tokenService.getAdminToken(payload)
-    const encodedToken = encodeURIComponent(token)
+    const acceptToken = tokenService.getAdminToken({...payload, action: REQUEST_ACCEPT_ACTION})
+    const rejectToken = tokenService.getAdminToken({...payload, action: REQUEST_REJECT_ACTION})
+    const encodedAcceptToken = encodeURIComponent(acceptToken)
+    const encodedRejectToken = encodeURIComponent(rejectToken)
 
-    await sendMessage(topics.email, 'request_access', JSON.stringify({sender: email, token: encodedToken}))
+    await sendMessage(topics.email, 'request_access', JSON.stringify({sender: email, acceptToken: encodedAcceptToken, rejectToken: encodedRejectToken}))
     .catch(error => {
         console.error(error)
         throw new Error(error.message)
