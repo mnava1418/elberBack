02792c44750ce62bf3522a9f81b19187cbfddda7
mnava1418 <martin@mnavapena.com>
2024-04-24 18:55:03 -0400

    ADD: service to send emails

--- email-services/src/services/userService.js
+++ email-services/src/services/userService.js
@@ -3,6 +3,7 @@ const { from } = require('../config').email
 
 const requestRegistrationCode = (message) => {
     emailService.sendEmail(from, 'NEW - Registration Request', message)
+    console.log('se mando')
 }
 
 module.exports = {
