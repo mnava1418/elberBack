d10dcf10ed90440a34352deee6f7670f646c90ec
mnava1418 <martin@mnavapena.com>
2024-04-27 19:19:04 -0400

    ADD: process registration code response

--- email-services/src/services/messageService.js
+++ email-services/src/services/messageService.js
@@ -2,18 +2,22 @@ const {topics, consumeMessagesFromTopic} = require('kafka-services')
 const userService = require('./userService')
 
 const processMessage = (key, message) => {
-    console.info('Kafka Message', key, message)
+    try {
+        console.info('Kafka Message', key, message)
 
-    switch (key) {
-        case 'request_access':
-            userService.requestRegistrationCode(message)
-            break;
-        case 'response_access':
-            userService.responseRegistrationCode(message)
-            break;
-        default:
-            console.info('Unable to identify message type')
-            break;
+        switch (key) {
+            case 'request_access':
+                userService.requestRegistrationCode(message)
+                break;
+            case 'response_access':
+                userService.responseRegistrationCode(message)
+                break;
+            default:
+                console.info('Unable to identify message type')
+                break;
+        }    
+    } catch (error) {
+        console.error(error)
     }
 }
 
