1b3dd1fc81f3700b80d3cd19b9cbe281ec717acd
mnava1418 <martin@mnavapena.com>
2024-04-27 17:58:09 -0400

    UPDATE: refactor

--- auth-services/src/app.js
+++ auth-services/src/app.js
@@ -22,7 +22,7 @@ const setMiddlewares = () => {
   //app middlewares
   app.use(limiter);
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
 }
