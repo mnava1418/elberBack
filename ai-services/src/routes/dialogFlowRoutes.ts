cdb62ee2695afdc2a86143b85dabc4aa8559f946
mnava1418 <martin@mnavapena.com>
2024-11-06 18:21:34 -0500

    ADD: middleware to validate firebase token

--- ai-services/src/routes/dialogFlowRoutes.ts
+++ ai-services/src/routes/dialogFlowRoutes.ts
@@ -1,8 +1,9 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
 import dialogFlowController from '../controllers/dialogFlowController';
+import authController from '../controllers/authController';
 
 const router = Router();
 
-router.post('/', dialogFlowController.sendMessage)
+router.post('/', authController.validateToken, dialogFlowController.sendMessage)
 
 export default router;
