9f48af31796026ad1dc0ad3c53c1762abb739471
mnava1418 <martin@mnavapena.com>
2025-04-01 21:25:29 -0400

    UPDATE: remove dialog flow endpoints. No needed

--- ai-services/src/routes/dialogFlowRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import dialogFlowController from '../controllers/dialogFlowController';
-import authController from '../controllers/authController';
-
-const router = Router();
-
-router.post('/', authController.validateToken, dialogFlowController.sendMessage)
-
-export default router;
--- ai-services/src/routes/index.ts
+++ ai-services/src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import dialogFlowRoutes from './dialogFlowRoutes'
 import chatRoutes from './chatRoutes'
 
 const router = Router();
@@ -8,7 +7,6 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello, world!');
 });
 
-router.use('/dialog', dialogFlowRoutes)
 router.use('/chat', chatRoutes)
 
 export default router;
