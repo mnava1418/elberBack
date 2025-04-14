965ab4641af3cf3693bd7612e29ed53bddca2cfd
mnava1418 <martin@mnavapena.com>
2025-04-13 20:13:53 -0400

    UPDATE: refactor code

--- ai-services/src/bin/localInteraction.ts
+++ ai-services/src/bin/localInteraction.ts
@@ -1,11 +1,11 @@
 import * as readlineSync from 'readline-sync';
-import generateResponse from '../services/nlpService';
+import { generateResponse } from '../services/nlpService';
 
 const local_interaction = async() => {
     while (true) {
         let pregunta = readlineSync.question('Martin: ')
         const response = await generateResponse(pregunta)
-        console.log('Elber:', response)
+        console.info('Elber:', response)
     }
 }
 
