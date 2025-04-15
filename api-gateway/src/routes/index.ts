7d968273daedc2b069d0e0ff6512b830b0ac883a
mnava1418 <martin@mnavapena.com>
2025-04-14 20:45:09 -0400

    ADD: routing to ai services

--- api-gateway/src/routes/index.ts
+++ api-gateway/src/routes/index.ts
@@ -1,9 +1,17 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import paths from '../config/paths';
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.render('index', { title: 'Elber API Gateway' });
 });
+
+router.use('/ai', createProxyMiddleware({
+  target: paths.ai_services,
+  changeOrigin: true,
+  pathRewrite: { '/ai': '/'}
+}))
 
 export default router;
