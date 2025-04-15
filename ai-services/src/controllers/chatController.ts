2a0000b5f00d3621fc25de92e3d03b6ac6c1727e
mnava1418 <martin@mnavapena.com>
2025-04-15 18:33:50 -0400

    UPDATE: refactor and get uid from headers

--- ai-services/src/controllers/chatController.ts
+++ ai-services/src/controllers/chatController.ts
@@ -1,47 +1,75 @@
 import {Response} from 'express'
-import { AuthenticationRequest } from "../interfaces/dialogFlowInterface";
 import * as chatService from '../services/chatService'
 import { ChatResponse } from '../interfaces/chatInterface';
+import { AuthenticationRequest, CustomHttpHeaders } from '../interfaces/httpInterface';
 
 export const getMessages = async (req: AuthenticationRequest, res: Response) => {
-    let{lastKey} = req.query
-    const user = req.user
-    
-    await chatService.getMessages(user!.uid, lastKey ? lastKey.toString() : null)
-    .then((response:ChatResponse) => {
-        res.status(200).json(response)
-    })
-    .catch((error: Error) => {
-        res.status(500).json({error: error.message})
-    })
+    try {
+        let{lastKey} = req.query
+        const headers = req.headers as CustomHttpHeaders
+        const uid = headers['x-user-uid']
+
+        if(!uid) {
+            res.status(500).json({error: 'User UID not provided.'})
+            return
+        }
+                
+        await chatService.getMessages(uid, lastKey ? lastKey.toString() : null)
+        .then((response:ChatResponse) => {
+            res.status(200).json(response)
+        })
+        .catch((error: Error) => {
+            res.status(500).json({error: error.message})
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Internal Error Server.'})
+    }
 }
 
 export const deleteMessages = async(req: AuthenticationRequest, res: Response) => {
-    const messageId = req.params.messageId
-    const user = req.user
+    try {
+        const messageId = req.params.messageId
+        const headers = req.headers as CustomHttpHeaders
+        const uid = headers['x-user-uid']
 
-    await chatService.deleteMessages(user!.uid, messageId ? messageId.toString() : null)
-    .then(() => {
-        res.status(200).json({})
-    })
-    .catch((error: Error) => {
-        res.status(500).json({error: error.message})
-    })
-}
+        if(!uid) {
+            res.status(500).json({error: 'User UID not provided.'})
+            return
+        }
 
-export const setIsFavorite = async (req: AuthenticationRequest, res: Response) => {
-    const {messageId, isFavorite} = req.body
-    const user = req.user
-
-    if(messageId !== undefined && isFavorite !== undefined && user !== undefined) {
-        await chatService.setIsFavorite(user.uid, messageId, isFavorite)
+        await chatService.deleteMessages(uid, messageId ? messageId.toString() : null)
         .then(() => {
             res.status(200).json({})
         })
         .catch((error: Error) => {
             res.status(500).json({error: error.message})
         })
-    } else {
-        res.status(500).json({error: 'Unable to process your request. Missing data.'})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Internal Error Server.'})
+    }
+}
+
+export const setIsFavorite = async (req: AuthenticationRequest, res: Response) => {
+    try {
+        const {messageId, isFavorite} = req.body
+        const headers = req.headers as CustomHttpHeaders
+        const uid = headers['x-user-uid']
+        
+        if(messageId !== undefined && isFavorite !== undefined && uid !== undefined) {
+            await chatService.setIsFavorite(uid, messageId, isFavorite)
+            .then(() => {
+                res.status(200).json({})
+            })
+            .catch((error: Error) => {
+                res.status(500).json({error: error.message})
+            })
+        } else {
+            res.status(500).json({error: 'Unable to process your request. Missing data.'})
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Internal Error Server.'})
     }
 }
