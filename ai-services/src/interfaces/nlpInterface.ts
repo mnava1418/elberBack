612732d167f91642d7c391e52406885b25117756
mnava1418 <martin@mnavapena.com>
2025-05-08 18:21:25 -0600

    UPDATE: generate text response for general inquiry

--- ai-services/src/interfaces/nlpInterface.ts
+++ ai-services/src/interfaces/nlpInterface.ts
@@ -1,7 +1,15 @@
 //FORMAT: elber_category_intentName
-export type IntentName = 'elber_general_fallback' | 'elber_general_name'
-export type IntentCategory = 'general'
+export enum IntentName {
+    ELBER_GENERAL_FALLBACK = 'elber_general_fallback',
+    ELBER_GENERAL_NAME = 'elber_general_name',
+    ELBER_GENERAL_MOTIVATION = 'elber_general_motivation',
+    ELBER_GENERAL_SKILLS = 'elber_general_skills'
+}
 
+export enum IntentCategory {
+    GENERAL = 'general'
+}
+    
 export type NLPIntents = {
     keywordRules: KeywordRule[],
 }
@@ -16,6 +24,16 @@ export type KeywordRule = {
     intent: IntentName
 }
 
-export type NLPResponse = {
+export enum NLPActions {
+    SHOW_TEXT = 'show_text',
+    PLAY_AUDIO = 'play_audion'
+}
+
+export type GeneralPayload = {
     text: string
+}
+
+export type NLPResponse = {
+    action: NLPActions,
+    payload: GeneralPayload
 }
